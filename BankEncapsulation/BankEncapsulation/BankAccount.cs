008a18f08525aa350e7efa099bb36b78223c6b2d
Matt Hart <mp.hart047@gamil.com>
2021-04-02 19:23:01 -0500

    BankAccount first push

---
+++ BankEncapsulation/BankEncapsulation/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    class BankAccount
+    {
+        private double _balance;
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Your money in the amount of {amount, 0:c} is being deposited");
+            _balance = amount;
+
+        }
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+}
---
+++ BankEncapsulation/BankEncapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+            account.Deposit(amountToDeposit);
+
+            double userBalance = account.GetBalance();
+            Console.WriteLine($"Your current account balance is {userBalance, 0:c}");
+        }
+    }
+}
